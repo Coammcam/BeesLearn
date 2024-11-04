e9777fc1878447dcf51f3650d10fbd172d672412
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-11-04 08:18:05 +0700

    Add retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,6 @@ dependencies {
     implementation("io.coil-kt:coil-gif:2.5.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     kapt("com.github.bumptech.glide:compiler:4.12.0")
-
-
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
