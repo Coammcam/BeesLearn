fc752990199d525fa8eefc7469360d3adf89a5ab
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-11-26 15:12:36 +0700

    Change variables in QuestionResponseModel.kt to be nullable

--- app/src/main/java/fpl/md07/beeslearn/models/responseModel/QuestionResponseModel.kt
+++ app/src/main/java/fpl/md07/beeslearn/models/responseModel/QuestionResponseModel.kt
@@ -6,7 +6,7 @@ import fpl.md07.beeslearn.models.TrueFalseQuestionModel_A
 import fpl.md07.beeslearn.models.Word
 
 data class QuestionResponseModel(
-    val words: List<Word>,
-    val trueFalseQuestions: List<TrueFalseQuestionModel_A>,
-    val grammarQuestions: List<GrammarQuestionModel>
+    val words: List<Word>?,
+    val trueFalseQuestions: List<TrueFalseQuestionModel_A>?,
+    val grammarQuestions: List<GrammarQuestionModel>?,
 )
