1f67a86b103c8953493a5ba8d3df0b0ac7594709
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2024-12-06 12:01:58 +0700

    Add reduce lives function and exit when lives count hits 0

--- app/src/main/java/fpl/md07/beeslearn/components/ConfirmQuestionNo.kt
+++ app/src/main/java/fpl/md07/beeslearn/components/ConfirmQuestionNo.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,19 +17,30 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
+import androidx.lifecycle.viewmodel.compose.viewModel
 import fpl.md07.beeslearn.ui.theme.Nunito_Bold
+import fpl.md07.beeslearn.viewmodels.UserDataViewModel
 
 @Composable
-fun ConfirmQuestionNo(Continue: () -> Unit) {
+fun ConfirmQuestionNo(`continue`: () -> Unit, exitWhenNotEnoughHive: () -> Unit) {
+    val userDataViewModel: UserDataViewModel = viewModel()
+    val currencyData = userDataViewModel.currencyData.value
+
+    LaunchedEffect(Unit) {
+        if(currencyData!!.honeyComb != 0){
+            currencyData.honeyComb -= 1
+            userDataViewModel.updateCurrencyData(currencyData)
+        }
+    }
+
     Box(
         modifier = Modifier
             .padding(top = 15.dp)
@@ -104,7 +114,13 @@ fun ConfirmQuestionNo(Continue: () -> Unit) {
 //            Spacer(modifier = Modifier.height(24.dp))
 
             Button(
-                onClick = { Continue() },
+                onClick = {
+                    if(currencyData!!.honeyComb == 0){
+                        exitWhenNotEnoughHive()
+                    }else{
+                        `continue`()
+                    }
+                          },
                 colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF5A5A)),
                 shape = RoundedCornerShape(8.dp),
                 modifier = Modifier
@@ -125,7 +141,8 @@ fun ConfirmQuestionNo(Continue: () -> Unit) {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun PreviewConfirmQuestion() {
-    ConfirmQuestionNo(){
-
-    }
+    ConfirmQuestionNo(
+        `continue` = {},
+        exitWhenNotEnoughHive = {}
+    )
 }
