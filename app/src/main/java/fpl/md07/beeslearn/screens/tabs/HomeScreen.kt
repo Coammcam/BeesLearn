240dc9b7486dba16d0f748e47e6add4701edacaf
Coammcam <hungnguyenisme84@gmail.com>
2024-12-16 16:30:32 +0700

    fix dot size in calendar

--- app/src/main/java/fpl/md07/beeslearn/screens/tabs/HomeScreen.kt
+++ app/src/main/java/fpl/md07/beeslearn/screens/tabs/HomeScreen.kt
@@ -431,7 +431,7 @@ fun RealCalendarView(context: Context, userDataViewModel: UserDataViewModel = vi
                                 Box(
                                     modifier = Modifier
                                         .align(Alignment.BottomCenter)
-                                        .size(6.dp)
+                                        .size(4.dp)
                                         .background(Color.Red, shape = CircleShape)
                                 )
                             }
