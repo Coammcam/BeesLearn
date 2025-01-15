3f7f23c730905979781d41999955341f41eee7de
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2025-01-15 16:10:45 +0700

    add getLevels(), getPartsOfLevel(), update getRandomQuestions() to use new api endpoint

--- app/src/main/java/fpl/md07/beeslearn/viewmodels/QuestionViewModel.kt
+++ app/src/main/java/fpl/md07/beeslearn/viewmodels/QuestionViewModel.kt
@@ -1,39 +1,86 @@
 package fpl.md07.beeslearn.viewmodels
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import fpl.md07.beeslearn.api.ApiService
 import fpl.md07.beeslearn.api.HttpRequest
+import fpl.md07.beeslearn.models.Level
+import fpl.md07.beeslearn.models.PartOfLevel
 import fpl.md07.beeslearn.models.responseModel.QuestionResponseModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlin.math.floor
 
 class QuestionViewModel : ViewModel() {
 
     private val apiService = HttpRequest.getInstance()
 
+    private val _levels = MutableLiveData<ArrayList<Level>>()
+    val levels: LiveData<ArrayList<Level>> = _levels
+
+    private val _partOfLevel = MutableLiveData<ArrayList<PartOfLevel>>()
+    val partOfLevel: LiveData<ArrayList<PartOfLevel>> = _partOfLevel
+
     fun getRandomQuestions(
-        total: Int, onSuccess: (QuestionResponseModel) -> Unit, onError: (String) -> Unit
+        level: Int, total: Int, onSuccess: (QuestionResponseModel) -> Unit, onError: (String) -> Unit
     ) {
+        val firstHalf = floor(total.toFloat() * 0.6f)
+        val secondHalf = total.toFloat() - firstHalf
+        val excess = total.toFloat() - (firstHalf + floor(secondHalf/2)*2)
+
+        val grammarAmount = (firstHalf + excess).toInt()
+        val wordAmount = floor(secondHalf/2).toInt()
+        val trueFalseAmount = floor(secondHalf/2).toInt()
+
         viewModelScope.launch {
             try {
-                val response = withContext(Dispatchers.IO) {
-                    apiService.getRandomQuestions(total)
-//                    Log.d("data", response)
-                }
-                Log.d("getRandomQuestions data", response.toString())
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        onSuccess(it)
-                    } ?: onError("No data received.")
-                } else {
-                    onError("Error: ${response.errorBody()?.string()}")
-                }
-            } catch (e: Exception) {
-                onError("Exception: ${e.message}")
+                val grammarQuestions = apiService.getGrammarByAmount(level, grammarAmount).body()
+                val trueFalseQuestions = apiService.getTrueFalseByAmount(level, trueFalseAmount).body()
+                val words = apiService.getWordByAmount(level, wordAmount*4).body()
+
+                onSuccess(
+                    QuestionResponseModel(
+                        words = words,
+                        trueFalseQuestions = trueFalseQuestions,
+                        grammarQuestions = grammarQuestions
+                    )
+                )
+
+            }catch (e: Exception){
+                println(e)
+                onError(e.toString())
             }
         }
     }
+
+    fun getLevels(){
+        viewModelScope.launch {
+            try {
+                val response = apiService.getLevels()
+                if(response.code() == 200){
+                    _levels.postValue(response.body())
+                }else{
+                    _levels.postValue(ArrayList<Level>())
+                }
+            }catch (e: Exception){
+                println(e)
+            }
+        }
+    }
+
+    fun getPartsOfLevel(level: Int){
+        viewModelScope.launch {
+            try {
+                val response = apiService.getPartOfLevel(level)
+                if(response.code() == 200){
+                    _partOfLevel.postValue(response.body())
+                }else{
+                    _partOfLevel.postValue(ArrayList<PartOfLevel>())
+                }
+            }catch (e: Exception){
+                println(e)
+            }
+        }
+    }
+
 }
