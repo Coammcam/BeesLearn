b1f2ffb179896a42aca77236ef58796ee7c051db
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2025-01-15 15:58:45 +0700

    change TrueFalseQuestionModel_A.kt to TrueFalseQuestionModel.kt

--- app/src/main/java/fpl/md07/beeslearn/models/TrueFalseQuestionModel.kt
+++ app/src/main/java/fpl/md07/beeslearn/models/TrueFalseQuestionModel.kt
@@ -1,7 +1,9 @@
 package fpl.md07.beeslearn.models
 
-// Data model for True/False question
 data class TrueFalseQuestionModel(
-    val questionText: String,  // The text of the question
-    val isTrue: Boolean        // True if the answer is true, False if the answer is false
+    val content: String,
+    val answer: String,
+    val vietnameseMeaning: String,
+    val correction: String,
+    val topic: String
 )
--- app/src/main/java/fpl/md07/beeslearn/models/TrueFalseQuestionModel_A.kt
+++
@@ -1,9 +0,0 @@
-package fpl.md07.beeslearn.models
-
-data class TrueFalseQuestionModel_A(
-    val content: String,
-    val answer: String,
-    val vietnameseMeaning: String,
-    val correction: String,
-    val topic: String
-)
