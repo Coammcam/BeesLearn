e46c7e8b35d4de9aa82f6814232ba68c0db45d60
Hoangdus <71692001+Hoangdus@users.noreply.github.com>
2025-01-15 16:16:33 +0700

    change TrueFalseQuestionModel_A to TrueFalseQuestionModel

--- app/src/main/java/fpl/md07/beeslearn/models/responseModel/QuestionResponseModel.kt
+++ app/src/main/java/fpl/md07/beeslearn/models/responseModel/QuestionResponseModel.kt
@@ -2,11 +2,10 @@ package fpl.md07.beeslearn.models.responseModel
 
 import fpl.md07.beeslearn.models.GrammarQuestionModel
 import fpl.md07.beeslearn.models.TrueFalseQuestionModel
-import fpl.md07.beeslearn.models.TrueFalseQuestionModel_A
 import fpl.md07.beeslearn.models.Word
 
 data class QuestionResponseModel(
     val words: List<Word>?,
-    val trueFalseQuestions: List<TrueFalseQuestionModel_A>?,
+    val trueFalseQuestions: List<TrueFalseQuestionModel>?,
     val grammarQuestions: List<GrammarQuestionModel>?,
 )
